a41c68a5b52e38e46d898cb352ae90c6e42647d8
adrianhamel <ahamel@hawk.iit.edu>
2023-10-29 22:56:34 -0500

    Added JS logic and updated index.html

--- Lab3/script/script.js
+++ Lab3/script/script.js
@@ -6,7 +6,21 @@ const tipRangeInput = document.getElementById("tipRange");
 const errorDiv = document.getElementById("error");
 
 function tipValues() {
+    const billTotal = parseFloat(billTotalInput.value);
+    const tipPercentage = tipRangeInput.value;
     
+    if (isNaN(billTotal)) {
+        errorDiv.textContent = "Please enter a valid number for Bill Total.";
+        tipPercentageInput.value = "";
+        tipAmountInput.value = "";
+        totalWithTipInput.value = "";
+    } else {
+        errorDiv.textContent = "";
+        tipPercentageInput.value = tipPercentage + "%";
+        const tipAmount = (billTotal * tipPercentage) / 100;
+        tipAmountInput.value = tipAmount.toFixed(2);
+        totalWithTipInput.value = (billTotal + tipAmount).toFixed(2);
+    }
 }
 
 billTotalInput.addEventListener("input", tipValues);
